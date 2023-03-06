1e434f3ded97cf5c80f1361a234f90e6aa76993e
Baakal Tesfaye <baakal@gmail.com>
2023-03-06 15:46:32 +0300

    Add Percentage of Household Enumerated Add Percentage of Population Enumerated Add Population Enumerated Add Population Enumerated by Day Add Population Enumerated by Day Cumulative

---
+++ Performance/PopulationEnumeratedAgainstTarget.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\IndicatorTemplates\Performance;
+
+use Carbon\Carbon;
+use Illuminate\Support\Collection;
+use Uneca\Chimera\Http\Livewire\Chart;
+use Uneca\Chimera\Interfaces\BarChart;
+use Uneca\Chimera\Services\AreaTree;
+use Uneca\Chimera\Traits\FilterBasedAxisTitle;
+
+class PopulationEnumeratedAgainstTarget extends Chart implements BarChart
+{
+    use FilterBasedAxisTitle;
+    private bool $isSampleData = false;
+    public function getData(array $filter = []): Collection
+    {
+        $this->isSampleData = true;
+        return collect([
+            (object)[
+                'area_name' => 'Area 1',
+                'area_code' => '1',
+                'total' => '1000',
+                'partial' => '1',
+                'special_case' => '0'
+
+            ],
+            (object) [
+                'area_name' => 'Area 2',
+                'area_code' => '2',
+                'total' => '1500',
+                'partial' => '1',
+                'special_case' => '0'
+            ],
+            (object) [
+                'area_name' => 'Area 3',
+                'area_code' => '3',
+                'total' => '1100',
+                'partial' => '1',
+                'special_case' => '0'
+            ],
+            (object) [
+                'area_name' => 'Area 4',
+                'area_code' => '4',
+                'total' => '900',
+                'partial' => '1',
+                'special_case' => '0'
+
+            ],
+            (object) [
+                'area_name' => 'Area 5',
+                'area_code' => '5',
+                'total' => '800',
+                'partial' => '1',
+                'special_case' => '0'
+
+            ],
+        ]);
+    }
+
+    protected function getTraces(Collection $data, string $filterPath): array
+    {
+        $areas = (new AreaTree())->areas($filterPath, nameOfReferenceValueToInclude: 'population');
+        if($this->isSampleData){
+            $areas = $data->map(function ($item) {
+                return (object) [
+                    'code' => $item->area_code,
+                    'name' => $item->area_name,
+                    'value' => \round($item->total * (1 +rand(1, 100) / 100)),
+                ];
+            });
+        }
+
+        $questionnaire = $this->indicator->getQuestionnaire();
+        // $startDate = $questionnaire->start_date->subDays(1);
+        $endDate = $questionnaire->end_date;
+        
+        $todayDate = Carbon::now()->format('Y-m-d');
+        if($todayDate > $endDate->format('Y-m-d')){
+            $todayDate = $endDate->format('Y-m-d');
+        }
+        // $daysSinceEnumStart = $startDate->diffInDays($todayDate,false);
+        // $totalDays = $startDate->diffInDays($endDate,false);
+
+        $dataKeyByAreaCode = $data->keyBy('area_code');
+        $data = $areas->map(function ($area) use ($dataKeyByAreaCode) {
+            $area->expected = $area->value;
+            $area->bar_width = 0.7;
+            $area->total = $dataKeyByAreaCode[$area->code]->total ?? 0;
+            $area->partial = $dataKeyByAreaCode[$area->code]->partial ?? 0;
+            $area->special_case = $dataKeyByAreaCode[$area->code]->special_case ?? 0;
+            return $area;
+        });
+
+        $traceTodayTarget = array_merge(
+            $this::ValueTraceTemplate,
+            [
+                'x' => $data->pluck('name')->all(),
+                'y' => $data->pluck('expected')->all(),  
+                'texttemplate' => "%{value:.0f}",
+                'hovertemplate' => "%{label}<br> %{value:.0f}",
+                'textposition' => 'outside',
+                'name' => "Today's target",
+                'marker' => ['color' => '#c5c5c5','opacity'=>0.4],
+            ]
+        );
+
+        $traceActual = array_merge(
+            $this::ValueTraceTemplate,
+            [
+                'x' => $data->pluck('name')->all(),
+                'y' => $data->pluck('total')->all(),
+                'texttemplate' => "%{value:.0f}",
+                'hovertemplate' => "%{label}<br> %{value:.0f}",
+                'width' => $data->pluck('bar_width')->all(),
+                'name' => 'Enumerated',
+                'marker' => ['color' => '#1e3b87'],
+            ]
+        );
+
+        return [$traceTodayTarget, $traceActual];
+   
+    }
+
+    protected function getLayout(string $filterPath): array
+    {
+        $layout = parent::getLayout($filterPath);
+        $layout['xaxis']['title']['text'] = $this->getAreaBasedAxisTitle($filterPath);
+        $layout['yaxis']['title']['text'] = "# of persons";
+        $layout['barmode'] = 'overlay';
+        if ($this->isSampleData) {
+            $layout['annotations'] = [[
+                'text' => __('SAMPLE'),
+                'textangle' => -30,
+                'opacity' => 0.12,
+                'xref' => 'paper',
+                'yref' => 'paper',
+                'font' => ['color' => 'black', 'size' => 120]
+            ]];
+        }
+        return $layout;
+    }
+}
---
+++ Performance/PopulationEnumeratedByDayCumulative.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\IndicatorTemplates\Performance;
+
+use Carbon\Carbon;
+use Illuminate\Support\Collection;
+use Uneca\Chimera\Http\Livewire\Chart;
+use Uneca\Chimera\Interfaces\LineChart;
+use Uneca\Chimera\Services\AreaTree;
+use Uneca\Chimera\Traits\FilterBasedAxisTitle;
+use Uneca\Chimera\Services\Helpers;
+
+class PopulationEnumeratedByDayCumulative  extends Chart implements LineChart
+{
+    use FilterBasedAxisTitle;
+    private bool $isSampleData = false;
+    public function getData(array $filter = []): Collection
+    {
+        $this->isSampleData = true;
+        return \collect([
+            (object)[
+                'enumeration_date' => Carbon::now()->subDays(4)->format('Y-m-d'),
+                'nice_date' => Carbon::now()->subDay(4)->format('m d,yyyy'), //'Jan 1, 2021',
+                'total' => '1000',
+                'cumulative_sum' => '1000',
+                'target' => '1000',
+            ],
+
+        ]);
+    }
+
+    protected function getTraces(Collection $data, string $filterPath): array
+    {
+
+        $now = Carbon::now();
+        $questionnaire = $this->indicator->getQuestionnaire();
+        $start_date = $questionnaire->start_date;
+        $end_date = $questionnaire->end_date;
+
+        $totalDays = $end_date->diffInDays($start_date) + 1; 
+        $areas = (new AreaTree())->areas($filterPath, nameOfReferenceValueToInclude: 'number_of_hh');
+        if($this->isSampleData){
+            $areas = $data->map(function ($item) use ($data) {
+                return (object) [
+                    'value' => \round($data->sum('total')),
+                ];
+            });
+        }
+        $householdEV = Helpers::safeDivide($areas->sum('value'), $totalDays);
+
+        $data = $data->map(function ($row)  use ($householdEV, $totalDays, $start_date, $end_date) {
+            if(Carbon::parse($row->enumeration_date)->lt($start_date)){
+                $row->target = 0;
+            } else {
+                $performedDays = Carbon::parse($row->enumeration_date)->diffInDays($start_date) + 1;
+                $performedDays = $performedDays > $totalDays ? $totalDays : $performedDays;
+                $row->target = $householdEV * $performedDays;
+            }
+            return $row;
+        });
+
+        $traceCumulative = array_merge(
+            $this::LineTraceTemplate,
+            [
+                'x' => $data->pluck('nice_date')->all(),
+                'y' => $data->pluck('cumulative_sum')->all(),
+                'texttemplate' => "%{value:.2f}",
+                'hovertemplate' => "%{label}<br> %{value:.0f}",
+                'name' => 'Actual',
+            ]
+        );
+        $traceTarget = array_merge(
+            $this::LineTraceTemplate,
+            [
+                'x' => $data->pluck('nice_date')->all(),
+                'y' => $data->pluck('target')->all(),
+                'texttemplate' => "%{value:.2f}",
+                'hovertemplate' => "%{label}<br> %{value:.0f}",
+                'name' => 'Target',
+            ]
+        );
+
+        return [$traceCumulative, $traceTarget];
+    }
+
+    protected function getLayout(string $filterPath): array
+    {
+        $layout = parent::getLayout($filterPath);        
+        $layout['xaxis']['title']['text'] = "Enumeration dates";
+        $layout['yaxis']['title']['text'] = "# of persons (cumulative)";
+        $layout['colorway'] = ['#1e3b87', '#c99c25', '#6f066f', '#7a37aa', '#a30538', '#ff0506', '#dba61f', '#ff6f06', '#fea405', '#ffff05', '#a3d804', '#056e05', '#3939d9', '#0579cc'];
+        if ($this->isSampleData) {
+            $layout['annotations'] = [[
+                'text' => __('SAMPLE'),
+                'textangle' => -30,
+                'opacity' => 0.12,
+                'xref' => 'paper',
+                'yref' => 'paper',
+                'font' => ['color' => 'black', 'size' => 120]
+            ]];
+        }
+        return $layout;
+    }
+}
